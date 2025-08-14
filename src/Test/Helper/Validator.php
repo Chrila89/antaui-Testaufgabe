4d8d7485ee3aaf2a3e5cc20533ec63a184a683ad
Chris <BarogDMC@web.de>
2025-08-14 19:00:25 +0200

    delete vali

--- src/Test/Helper/Validator.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Test\Helper;
-
-class Validator {
-    public static function validateLoginData($username, $password) {
-        return !empty($username) && !empty($password);
-    }
-}
